2871c03d53d59e4c3f4dd0396a6ea40c73c3a561
DEVIL-X <72757203+TEAMOFDEVIL-X@users.noreply.github.com>
2021-07-26 15:20:15 +0530

    Create lyrics.py

---
+++ Tianabot/modules/lyrics.py
@@ -0,0 +1,24 @@
+from aiohttp import ClientSession
+from Python_ARQ import ARQ
+from Tianabot.config import ARQ_API_URL, ARQ_API_KEY
+
+from Tianabot import *
+from Tianabot.events import register
+
+aiohttpsession = ClientSession()
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+
+@register(pattern="^/lyrics (.*)")
+async def lyrics_func(_, message):
+    if len(message.command) < 2:
+        await message.reply_text("**Sike That's The Wrong Command Usage!** \nUse `/lyrics` (song name)")
+        return
+    m = await message.reply_text("**Searching For Song Lyrics**")
+    query = message.text.strip().split(None, 1)[1]
+    song = await arq.lyrics(query)
+    lyrics = song.result
+    if len(lyrics) < 4095:
+        await m.edit(f"__{lyrics}__")
+        return
+    lyrics = await paste(lyrics)
+    await m.edit(f"**Oops! Lyrics Too Long To Send!** \n**Your Song Lyrics: [Click Here]({lyrics})**")
